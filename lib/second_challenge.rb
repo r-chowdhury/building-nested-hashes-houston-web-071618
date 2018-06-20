39562639c2fd49829a677587e9a6ccce957f54db
ryhan.chowdhury3@gmail.com <ryhan.chowdhury3@gmail.com>
2018-06-20 13:26:21 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,7 +2,10 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      :patriarch => {},
+      :patriarch => {
+        :name => "Lord Montague",
+        :age => "54"
+      },
       :matriarch => {},
       :hero => {},
       :hero_friends => []
