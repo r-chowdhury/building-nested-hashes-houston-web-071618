1008840c0cebf295545c368016ed9138cacac7be
ryhan.chowdhury3@gmail.com <ryhan.chowdhury3@gmail.com>
2018-06-20 13:28:31 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -27,7 +27,11 @@ def second_challenge
         :name => "Lady Capulet",
         :age => "51"
       },
-      :heroine => {},
+      :heroine => {
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
+      },
       :heroine_friends => []
       }
     
